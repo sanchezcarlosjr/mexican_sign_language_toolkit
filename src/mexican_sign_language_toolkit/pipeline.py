0aeb64272c970109b04286ea9b3d4b516608133a
Carlos Eduardo Sanchez Torres (sanchezcarlosjr) <24639141+sanchezcarlosjr@users.noreply.github.com>
2023-10-04 22:09:25 -0700

    Update pipeline.py

--- src/mexican_sign_language_toolkit/pipeline.py
+++ src/mexican_sign_language_toolkit/pipeline.py
@@ -5,12 +5,42 @@ from mexican_sign_language_toolkit.lexer import tokenize
 import mediapipe as mp
 import cv2
 import time
+import requests
+import os
+
+def download_file(url, local_filename):
+    """
+    Download a file from the given URL and save it to the specified local filename.
+    :param url: The URL of the file to download
+    :param local_filename: The local file name to save the downloaded file
+    """
+    with requests.get(url, stream=True) as response:
+        # Check if the request was successful (status code 200)
+        response.raise_for_status()
+        # Write the content of the request to a local file
+        with open(local_filename, 'wb') as file:
+            for chunk in response.iter_content(chunk_size=8192):
+                file.write(chunk)
+                
+def install(*paths):
+    default_http_requests = [
+        "https://github.com/sanchezcarlosjr/mexican_sign_language_toolkit/raw/main/checkpoints/regex.npy",
+        "https://github.com/sanchezcarlosjr/mexican_sign_language_toolkit/raw/main/checkpoints/sign_language_space.npy"
+    ]
+    default_names = [
+        "regex.npy",
+        "sign_language_space.npy"
+    ]
+    for index, path in enumerate(paths):
+        if not os.path.exists(path):
+            download_file(default_http_requests[index], default_names[index])
 
 class Pipeline:
     """
     Base pipeline class for processing and predicting landmarks.
     """
-    def __init__(self, space_path='../checkpoints/sign_language_space.npy', regex_path='../checkpoints/regex.npy'):
+    def __init__(self, space_path='sign_language_space.npy', regex_path='regex.npy'):
+        install(space_path, regex_path)
         space = np.load(space_path, allow_pickle=True)
         regex = str(np.load(regex_path, allow_pickle=True))
         self.brute_force = Bruteforce(space)
