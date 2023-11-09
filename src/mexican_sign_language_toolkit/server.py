926184269875a67b0133bec544e395667be76b2e
sanchezcarlosjr <24639141+sanchezcarlosjr@users.noreply.github.com>
2023-11-08 17:30:05 -0800

    feat: solve new gradio version

--- src/mexican_sign_language_toolkit/server.py
+++ src/mexican_sign_language_toolkit/server.py
@@ -10,7 +10,7 @@ def predict(path):
 def launch_server():
   demo = gr.Interface(
     predict,
-    [gr.Video(source="webcam", mirror_webcam=False)],
+    [gr.Video(sources="webcam", mirror_webcam=False)],
     ["text"],
     examples=[*glob.glob('**/*.mp4', recursive=True),*glob.glob('**/*.mkv', recursive=True)],
     title="Mexican sign language recognizer demo",
